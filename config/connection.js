95563e3981bddceac3c5a0abc6acdec1caeb6ee4
rdheadyii <rdheadyii@gmail.com>
2023-08-08 17:35:46 -0400

    wrote connection js so sequelize can make a connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// require sequealize and dotenv 
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// make connection with sequelize
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+module.exports = sequelize;
