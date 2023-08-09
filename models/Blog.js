bb953eea05c4fb16ae8c0101c3c913079412bc92
rdheadyii <rdheadyii@gmail.com>
2023-08-09 15:00:33 -0400

    wrote schema and started writing Blog model

---
+++ models/Blog.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING
+        },
+    }
+)
